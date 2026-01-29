356126e43fdb77772d1ac1e889555f3009e66b03
Diana Shimlykh <d56671250@gmail.com>
2026-01-29 22:42:57 +0300

    add: script

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "Start";
+let screens = [1, 2, 3, 4];
+const screenPrice = 50;
+let rollback = 10;
+const fullPrice = 200;
+let adaptive = true;
+
+alert (`Full price is ${fullPrice}`);
+console.log(screens.forEach(screen => console.log(screen)));
