4b6f2633e708ca37e77121e89de8c6eecf597b87
Diana Shimlykh <d56671250@gmail.com>
2026-02-05 22:34:03 +0300

    add: lesson03 extra solution

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const title = "Start";
-let screens = [1, 2, 3, 4];
-const screenPrice = 50;
-let rollback = 10;
-const fullPrice = 200;
-let adaptive = true;
+'use strict';
+let lang = prompt("Какой язык?", 'ru или eng');
+const daysOfTheWeekRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+const daysOfTheWeekEng = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const days = {
+    ru: ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    eng: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+}
+const namePerson = prompt("Введите имя:", "Артем");
+//1st variant with if:
+// if(lang === 'ru'){
+//     console.log(daysOfTheWeekRu);
+// } else if (lang === 'eng') {
+//     console.log(daysOfTheWeekEng);
+// } else {
+//     console.log('unknown language')
+// };
 
-alert (`Full price is ${fullPrice}`);
-console.log(screens.forEach(screen => console.log(screen)));
+//variant 1.2 ternary operator: 
+// lang === 'ru' ? console.log(daysOfTheWeekRu) : console.log(daysOfTheWeekEng);
+
+//2nd variant with switch:
+// switch (lang) {
+//     case 'ru':
+//      console.log(daysOfTheWeekRu);
+//      break;
+     
+//      case 'eng':
+//      console.log(daysOfTheWeekEng);
+//      break;
+// }
+
+//3d variant:
+console.log(days[lang]);
+
+namePerson === "Артем" ? console.log("Директор") : 
+namePerson === "Александр" ? console.log("Преподаватель") :
+console.log("студент");
