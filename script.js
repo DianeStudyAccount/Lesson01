81e512733e6cbf0c49ce923b31b4f24f3dd7eb42
Diana Shimlykh <d56671250@gmail.com>
2026-02-06 22:22:26 +0300

    add:lesson5 extra task

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const title = "Start";
-let screens = [1, 2, 3, 4];
-const screenPrice = 50;
-let rollback = 10;
-const fullPrice = 200;
-let adaptive = true;
+"use strict";
 
-alert (`Full price is ${fullPrice}`);
-console.log(screens.forEach(screen => console.log(screen)));
+let arr = [
+  "2234342",
+  "6455",
+  "91821",
+  "44444",
+  "5858585",
+  "12345567789",
+  "4657",
+];
+arr.forEach((num) => {
+  if (num[0] === "2" || num[0] === "4") {
+    console.log(num);
+  }
+});
+
+//prime numbers
+for (let i = 2; i <= 100; i++) {
+  let count = 0;
+  for (let j = 2; j <= i && count < 2; j++) {
+    if (i % j === 0) {
+      ++count;
+    }
+  }
+  if (count < 2) {
+    console.log(i + ` Делители этого числа: 1 и ${i}`)
+  }
+}
+
+//or the second variant: 
+// for (let i = 2; i <= 100; i++) {
+//   let isPrime = true;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(i + ` Делители этого числа: 1 и ${i}`)
+//   }
+// }
+//Какой в принципе вариант предпочтителен? в итернете есть решения с решетом Эратосфена и другие, 
+// что проще для понимания и реализации? 
